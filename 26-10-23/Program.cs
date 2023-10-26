5ca3343351a31c494a0eb2bd808a88c9bb435b3d
gabrieltdrk <gabriel.andradesx@gmail.com>
2023-10-26 20:06:13 -0300

    classes bases e derivadas criadas

--- 26-10-23/Program.cs
+++ 26-10-23/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Ingresso
+{
+    float preco { get; set; }
+
+    public float imprimeValor()
+    {
+        return this.preco;
+    }
+
+}
+
+class Camarote_Inferior : Ingresso
+{
+    string localidade { get; set; }
+}
+class Camarote_Superior : Ingresso
+{
+
+}
+
+class Normal : Ingresso
+{
+
+}
+
+class VIP : Ingresso
+{
+    float adicionalVIP = 25;
+}
